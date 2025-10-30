ae4f1d6c582d00d79bb3d038cb54ed0d5bba8045
klu2300031386 <163740076+klu2300031386@users.noreply.github.com>
2025-10-30 11:50:32 +0530

    Fix import statements formatting in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import { CartProvider } from "./context/CartContext"
+import App from './App.jsx';
+import { CartProvider } from "./context/CartContext";
 
 createRoot(document.getElementById('root')).render(
   <CartProvider>
